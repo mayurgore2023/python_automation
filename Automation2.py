e6fab45b7ec63a349414a7c23ef2dd4ce5d16de0
MAYUR RAJENDRA GORE <125110603+mayurgore2023@users.noreply.github.com>
2023-02-22 18:54:30 +0530

    Create Automation2.py

---
+++ Automation2.py
@@ -0,0 +1,21 @@
+
+# application for task schedular 
+
+import schedule
+import time
+import datetime
+
+def Fun():
+    print("Inside fun")
+
+def main():
+        print("inside  task schedular")
+
+        schedule.every(1).minutes.do(Fun)
+
+        while (True):
+            schedule.run_pending()
+            time.sleep(1)
+
+if __name__=="__main__":
+    main()
